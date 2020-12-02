400288b17af97c2ab545be41d30d492b0f1c7fb3
Guido_Fe <guidoferris@gmail.com>
2020-12-02 23:21:00 +0100

    Fixed lines length

--- cat.sh
+++ cat.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ncol=`tput cols`
-t[0]='　∧＿∧ \n'
-t[1]=' ( ･ω･)\n'
+t[0]='　∧＿∧  \n'
+t[1]=' ( ･ω･) \n'
 t[2]='―∪――――∪―'
-t[4]='_______'
-t[5]=' |　　|\n'
-t[6]=' |　　|\n'
-t[7]='  U￣U\n'
+t[4]='________'
+t[5]=' |　　| \n'
+t[6]=' |　　| \n'
+t[7]='  U￣U  \n'
 
 nspaces=$(($ncol / 2 - 4))
 trailspaces=$(($ncol / 2 - 4))
@@ -17,25 +17,26 @@ do
   if [ $i -eq 3 ]; then
     cat $@
   else
+    car=""
+    if [ $i -eq 2 ]; then
+      car="―"
+    elif [ $i -eq 4 ]; then
+      car="_"
+    else
+      car=" "
+    fi
     for s in `seq 1 $nspaces`; do
-      if [ $i -eq 2 ]; then
-        printf "―"
-      elif [ $i -eq 4 ]; then
-        printf "_"
-      else
-        printf " "
-      fi
+      printf "$car"
     done
     printf "${t[$i]}"
     if [ $i -eq 2 -o $i -eq 4 ]; then
-      car=""
       if [ $i -eq 2 ]; then
         car="―"
       else
         car="_"
       fi
       for s in `seq 1 $trailspaces`; do
-        printf $car
+        printf "$car"
       done
       printf "\n"
     fi
